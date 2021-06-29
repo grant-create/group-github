211522051df7846cb59a9d014f1404da73ce7015
grant-create <grant.s.wilkie@gmail.com>
2021-06-29 14:33:09 -0700

    uhh

--- src/components/Shawn.jsx
+++ src/components/Shawn.jsx
@@ -2,7 +2,7 @@ export default function Shawn() {
     return(
         <div>
 
-            <img src='/ShawnFavAnimal.jpg' target='blank' width='500' alt='B fav animal'/>
+            <img src='/ShawnFavAnimal.jpeg' target='blank' width='500' alt='B fav animal'/>
             <p>My name is Shawn!</p>  
             <p> Software Engineer | Full Stack | Programmer at GA</p>
 
