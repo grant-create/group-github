c46fc07589a2a5c66cd59d2f0a32f4a2b9a45906
shawn-stover <shawn.stover.it.cs@gmail.com>
2021-06-29 14:32:41 -0700

    Changed Shawn.jsx

--- src/components/Shawn.jsx
+++ src/components/Shawn.jsx
@@ -1,7 +1,7 @@
 export default function Shawn() {
     return(
         <div>
-            <img src='/ShawnFavAnimal.png' target='blank' width='500' alt='B fav animal'/>
+            <img src='/ShawnFavAnimal.jpeg' target='blank' height='500'git add . width='500' alt='B fav animal'/>
             <p>My name is Shawn!</p>  
             <p> Software Engineer | Full Stack | Programmer at GA</p>
         </div>
