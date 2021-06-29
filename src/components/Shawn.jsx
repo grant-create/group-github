1ae592be650db55369127e5db39befd4441bc9d8
shawn-stover <shawn.stover.it.cs@gmail.com>
2021-06-29 14:22:49 -0700

    Shawn added

--- src/components/Shawn.jsx
+++ src/components/Shawn.jsx
@@ -1,5 +1,9 @@
 export default function Shawn() {
-    return{
-        
-    }
+    return(
+        <div>
+            <img src='/ShawnFavAnimal.png' target='blank' width='500' alt='B fav animal'/>
+            <p>My name is Shawn!</p>  
+            <p> Software Engineer | Full Stack | Programmer at GA</p>
+        </div>
+    )
 }
